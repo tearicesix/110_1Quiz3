aafa77470393e7d068486cf434fd33e7bcd8a060
偉翔 黃 <108111109@mail.aeust.edu.tw>
2021-12-03 11:17:49 +0800

    session

---
+++ 110_1Quiz3/Session.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Quiz3
+{
+    public partial class Session : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s_Name = "霸道總裁";
+            string s_Id = "A123456789";
+            Session["name"] = s_Name;
+            Session["id"] = s_Id;
+            Response.Write("Session 已註冊, 保存資訊有2 <br />");
+            Response.Write("保存客戶端姓名" + s_Name + "<br />");
+            Response.Write("保存客戶端ID" + s_Id + "<br />");
+            Response.Write("會議室ID" + Session.SessionID + "<br />");
+            Response.Write("生命週期" + Session.Timeout.ToString() + "分鐘 <br/>");
+        }
+    }
+}
---
+++ 110_1Quiz3/Session2.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Quiz3
+{
+    public partial class Session2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s_Name = "";
+            string s_Id = "";
+            if (Session["name"] != null)
+            {
+                Response.Write("會議室ID" + Session.SessionID + " <br / >");
+
+                s_Name = (string)Session["name"];
+                Response.Write("保存客戶端姓名" + s_Name + " <br />");
+            }
+            else
+            {
+                Response.Write("保存客戶端姓名-會議室已遺失<br />");
+            }
+            if (Session["id"] != null)
+            {
+                s_Id = (string)Session["id"];
+                Response.Write("保存客戶端ID" + s_Id + " <br />");
+            }
+            else
+            {
+                Response.Write("保存客戶端ID-會議室已遺失<br />");
+            }
+        }
+    }
+}
